67fa734623bc4ff5201110d0f53b3589292381c9
absaki <28054064+absaki@users.noreply.github.com>
2021-02-02 21:56:36 +0900

    [add]	new file:includes/minishell.h
    add header file to delare #define s.

---
+++ includes/minishell.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kikeda <kikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/02 21:54:31 by kikeda            #+#    #+#             */
+/*   Updated: 2021/02/02 21:56:09 by kikeda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <errno.h>
+# include <signal.h>
+
+# define SUCCESS 0
+# define ERROR 1
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
+# define BUFF_SIZE 1024
+
+#endif
