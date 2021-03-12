3bfffef329b61a7d95bddb71c6c5a81c9fb4a122
absaki <28054064+absaki@users.noreply.github.com>
2021-03-12 21:34:05 +0900

    [fix] : is a directory

--- srcs/exec/cmdsearch.c
+++ srcs/exec/cmdsearch.c
@@ -6,7 +6,7 @@
 /*   By: kikeda <kikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 13:49:27 by kike              #+#    #+#             */
-/*   Updated: 2021/03/08 00:33:54 by kikeda           ###   ########.fr       */
+/*   Updated: 2021/03/12 21:32:05 by kikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,16 @@ char    *serch_path(char *file, t_env *env)
     return (NULL);
 }
 
+int     is_directory(char *fullpath)
+{
+    struct stat stat_st;
+    
+    stat(fullpath, &stat_st);
+    if(S_ISDIR(stat_st.st_mode))
+        return (1);
+    return (0);
+}
+
 int     my_execvp(char *file, char **argv, t_sh *sh)
 {
     char *cmdpath;
@@ -63,6 +73,12 @@ int     my_execvp(char *file, char **argv, t_sh *sh)
         cmdpath = serch_path(file, sh->env);
     else
         cmdpath = file;
+    if(is_directory(cmdpath))
+    {
+        ft_putstr_fd(file, STDERR);
+	    ft_putendl_fd(": Is a directory", STDERR);
+        exit(126);
+    }
     envp = makeenvlist(sh->env);
     if (cmdpath)
         return (execve(cmdpath, argv, envp));
