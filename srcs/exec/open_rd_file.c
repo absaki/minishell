ef629b3bbaa85114d3304892c8ba61d3678b38cf
absaki <28054064+absaki@users.noreply.github.com>
2021-03-13 00:48:35 +0900

    [fix] stdin redirextion

--- srcs/exec/open_rd_file.c
+++ srcs/exec/open_rd_file.c
@@ -6,28 +6,28 @@
 /*   By: kikeda <kikeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 16:42:11 by kike              #+#    #+#             */
-/*   Updated: 2021/03/13 00:24:20 by kikeda           ###   ########.fr       */
+/*   Updated: 2021/03/13 00:47:53 by kikeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int open0(t_redirection *rd)
+static int openzero(t_redirection *rd)
 {
     int fd;
 
-    if ((fd = open(rd->file, O_RDONLY) != -1))
-        {
+    fd = open(rd->file, O_RDONLY);
+    if (fd != -1)
+    {
 		dup2(fd, 0);
-        ft_putnbr_fd(fd,2);
         close (fd);
         return (SUCCESS);
-        }
+    }
     ft_putstr_fd("No such file or directory\n", 2);
     return (ERROR);    
 }
 
-static int open1(t_redirection *rd)
+static int openone(t_redirection *rd)
 {
     int fd;
     int permisiion;
@@ -46,7 +46,7 @@ static int open1(t_redirection *rd)
     return (ERROR);    
 }
 
-static int open2(t_redirection *rd)
+static int opentwo(t_redirection *rd)
 {
     int fd;
     int permisiion;
@@ -76,11 +76,11 @@ int         set_fd(t_sh *sh)
 	{
 		rd = list->content;
 		if(rd->fd == 0)
-		    open_result = open0(rd);
+		    open_result = openzero(rd);
         else if(rd->fd == 1)
-		    open_result = open1(rd);
+		    open_result = openone(rd);
 		else if(rd->fd == 2)
-		    open_result = open2(rd);
+		    open_result = opentwo(rd);
 		else
 			break;
         if(open_result == ERROR)
